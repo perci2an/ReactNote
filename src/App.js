ab6d3d78df89247a38df0f5997c992eac23d7fab
perci2an <psmim1216@naver.com>
2023-12-06 01:19:25 +0900

    movie API

--- src/App.js
+++ src/App.js
@@ -1,32 +1,8 @@
 import { useEffect, useState } from "react";
+import Movie from "./components/Movie";
 
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
-  useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers")
-      .then((response) => response.json())
-      .then((json) => {
-        setCoins(json);
-        setLoading(false);
-      });
-  }, []);
-  return (
-    <div>
-      <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
-      {loading ? (
-        <strong>Loading...</strong>
-      ) : (
-        <select>
-          {coins.map((coin) => (
-            <option>
-              {coin.name} ({coin.symbol}): {coin.quotes.USD.price}USD
-            </option>
-          ))}
-        </select>
-      )}
-    </div>
-  );
+  return null;
 }
 
 export default App;
