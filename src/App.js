c0e5d877c9b50f5cacdea2500ee36148b2966795
perci2an <psmim1216@naver.com>
2023-12-06 12:27:36 +0900

    movie API design

--- src/App.js
+++
@@ -1,8 +0,0 @@
-import { useEffect, useState } from "react";
-import Movie from "./components/Movie";
-
-function App() {
-  return null;
-}
-
-export default App;
---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Home from "./routes/Home";
+import Detail from "./routes/Detail";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movie/:id" element={<Detail />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
